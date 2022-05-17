5aaefe93bbd6da34ea49d3a0511fd4bf55921477
mi-wada <mtak.wd@gmail.com>
2022-05-17 23:11:50 +0900

    add 🚀

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", routing::get(|| async { "Hello, World!🚀" }));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
